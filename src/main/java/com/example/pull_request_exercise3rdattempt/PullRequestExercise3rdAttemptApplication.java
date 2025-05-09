40036bca6600459203a75eb9f6d66f95d4b7f520
Daniel23894 <daso0005@stud.kea.dk>
2025-05-09 15:20:40 +0200

    Initial commit with hello.txt

---
+++ src/main/java/com/example/pull_request_exercise3rdattempt/PullRequestExercise3rdAttemptApplication.java
@@ -0,0 +1,13 @@
+package com.example.pull_request_exercise3rdattempt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestExercise3rdAttemptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullRequestExercise3rdAttemptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pull_request_exercise3rdattempt/PullRequestExercise3rdAttemptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pull_request_exercise3rdattempt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullRequestExercise3rdAttemptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
